3aaabc09a30cfb35cd51615d7eef730e16aeee71
kemal <kemal@DESKTOP-IKOMOKD>
2023-07-26 14:22:24 +0300

    Add project files.

---
+++ GCX/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using PureCloudPlatform.Client.V2.Model;
+
+Console.WriteLine("Hello, World!");
+
+PureCloudPlatform.Client.V2.Api.ArchitectApi api = new PureCloudPlatform.Client.V2.Api.ArchitectApi();
+Flow f = new Flow();
+ScheduleGroup sc = new ScheduleGroup();
+IVR i = new IVR();
+i.
